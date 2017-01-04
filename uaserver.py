f6589e5e1243821bb30f5fe7d6546af9e7f7608d
robernom <rober95@hotmail.com>
2017-01-04 12:51:54 +0100

    Terminada parte basica

--- uaserver.py
+++ uaserver.py
@@ -5,7 +5,7 @@
 import socketserver
 import sys
 from os import system
-from uaclient import UAHandler
+from uaclient import UAHandler, log
 
 RESP_COD = {100: 'SIP/2.0 100 Trying\r\n', 180: 'SIP/2.0 180 Ring\r\n',
             200: 'SIP/2.0 200 OK'}
@@ -14,24 +14,24 @@ class SIPHandler(socketserver.DatagramRequestHandler):
     """SIP server class."""
     def handle(self):
         data = self.request[0].decode('utf-8')
-        socket = self.request[1]
-        if data:
-            print(data)
-            met = data.split()[0]
-        else:
-            met = ""
+        print(data)
+        obj_log.log_write("recv", REGPROX, data)
+        met = data.split()[0]
         if met == "INVITE":
             to_send = (RESP_COD[100] + RESP_COD[180] + RESP_COD[200] + "\r\n"
                     + "Content-Type: application/sdp\r\n\r\nv=0\r\no={} {}\r\n"
                     + "s=Conver\r\nt=0\r\nm=audio {} RTP\r\n\r\n")
             to_send = to_send.format(NAME, SERVER[0], PORTP)
             self.wfile.write(bytes(to_send, 'utf-8'))
+            obj_log.log_write("send", REGPROX, to_send)
         elif met == "ACK":
             cmd = "./mp32rtp -i {} -p {} < {}"
+            obj_log.log_write("", "", "")
             system(cmd.format(SERVER[0], PORTP, AUD_PATH))
         elif met == "BYE":
             to_send = (RESP_COD[200] + "\r\n\r\n")
             self.wfile.write(bytes(to_send, 'utf-8'))
+            obj_log.log_write("send", REGPROX, to_send)
 
 if __name__ == "__main__":
     try:
@@ -45,8 +45,9 @@ if __name__ == "__main__":
               int(cHandler.config['uaserver']['puerto']))
     PORTP = cHandler.config['rtpaudio']['puerto']
     REGPROX = (cHandler.config['regproxy']['ip'],
-               int(cHandler.config['regproxy']['puerto']))
+               cHandler.config['regproxy']['puerto'])
     LOG_PATH = cHandler.config['log']['path']
+    obj_log = log(LOG_PATH)
     AUD_PATH = cHandler.config['audio']['path']
     SERV = socketserver.UDPServer(SERVER, SIPHandler)
     print("Listening...")
